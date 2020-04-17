739e6ed9d255ca866852773223552fbdf800adba
Lucas <lukasz.rohde@in4it.io>
2020-04-17 16:16:32 +0200

    add more controls

---
+++ config.tf
@@ -0,0 +1,91 @@
+# 2.5 – Ensure AWS Config is enabled 
+
+data "aws_iam_policy_document" "recorder_assume_role_policy" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "recorder" {
+  name               = "${var.resource_name_prefix}-config-role"
+  assume_role_policy = data.aws_iam_policy_document.recorder_assume_role_policy.json
+  tags               = var.tags
+}
+
+#https://docs.aws.amazon.com/config/latest/developerguide/iamrole-permissions.html
+data "aws_iam_policy_document" "recorder_publish_policy" {
+  statement {
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.audit[0].arn}/config/AWSLogs/${var.aws_account_id}/*"]
+
+    condition {
+      test     = "StringLike"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+
+  statement {
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.audit[0].arn]
+  }
+
+  statement {
+    actions = ["sns:Publish"]
+
+    resources = [var.sns_arn]
+  }
+}
+
+resource "aws_iam_role_policy" "recorder_publish_policy" {
+  name   = "${var.resource_name_prefix}-config-policy"
+  role   = aws_iam_role.recorder.id
+  policy = data.aws_iam_policy_document.recorder_publish_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "recorder_read_policy" {
+  role       = aws_iam_role.recorder.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+}
+
+resource "aws_config_configuration_recorder" "recorder" {
+  count = var.config_enabled ? 1 : 0
+
+  name = var.resource_name_prefix
+
+  role_arn = aws_iam_role.recorder.arn
+
+  recording_group {
+    all_supported                 = true
+    include_global_resource_types = var.include_global_resource_types
+  }
+}
+
+resource "aws_config_delivery_channel" "bucket" {
+  count = var.config_enabled ? 1 : 0
+
+  name = var.resource_name_prefix
+
+  s3_bucket_name = aws_s3_bucket.audit[0].arn
+  s3_key_prefix  = "config"
+  sns_topic_arn  = var.sns_arn
+
+  snapshot_delivery_properties {
+    delivery_frequency = "One_Hour"
+  }
+
+  depends_on = [aws_config_configuration_recorder.recorder[0]]
+}
+
+resource "aws_config_configuration_recorder_status" "recorder" {
+  count = var.config_enabled ? 1 : 0
+
+  name = aws_config_configuration_recorder.recorder[0].id
+
+  is_enabled = true
+  depends_on = [aws_config_delivery_channel.bucket[0]]
+}
---
+++ s3.tf
@@ -0,0 +1,127 @@
+data "aws_iam_policy_document" "audit_log" {
+  count = var.s3_enabled ? 1 : 0
+
+  override_json = var.audit_log_bucket_custom_policy_json
+
+  statement {
+    sid     = "AWSCloudTrailAclCheckForConfig"
+    actions = ["s3:GetBucketAcl"]
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+    resources = [aws_s3_bucket.audit[0].arn]
+  }
+
+  statement {
+    sid     = "AWSCloudTrailWriteForConfig"
+    actions = ["s3:PutObject"]
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+    resources = [
+      "${aws_s3_bucket.audit[0].arn}/config/AWSLogs/${var.aws_account_id}/Config/*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+
+  statement {
+    sid     = "AWSCloudTrailAclCheckForCloudTrail"
+    actions = ["s3:GetBucketAcl"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    resources = [aws_s3_bucket.audit[0].arn]
+  }
+  statement {
+    sid     = "AWSCloudTrailWriteForCloudTrail"
+    actions = ["s3:PutObject"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    resources = [
+      "${aws_s3_bucket.audit[0].arn}/cloudtrail/AWSLogs/${var.aws_account_id}/*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "audit_log" {
+  count  = var.s3_enabled ? 1 : 0
+  bucket = aws_s3_bucket.audit[0].id
+  policy = data.aws_iam_policy_document.audit_log[0].json
+}
+
+resource "aws_s3_bucket" "access_log" {
+  count  = var.s3_enabled ? 1 : 0
+  bucket = "${var.resource_name_prefix}-access-logs"
+  acl    = "log-delivery-write"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = var.tags
+}
+
+# 2.3 – Ensure the S3 bucket CloudTrail logs to is not publicly accessible
+# 2.6 – Ensure S3 bucket access logging is enabled on the CloudTrail S3 bucket
+resource "aws_s3_bucket_public_access_block" "access_log" {
+  count = var.s3_enabled ? 1 : 0
+
+  bucket = aws_s3_bucket.access_log[0].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "audit" {
+  count  = var.s3_enabled ? 1 : 0
+  bucket = "${var.resource_name_prefix}-audit-logs"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.access_log[0].id
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "audit" {
+  count = var.s3_enabled ? 1 : 0
+
+  bucket = aws_s3_bucket.audit[0].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
